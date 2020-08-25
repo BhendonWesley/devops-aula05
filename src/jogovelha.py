c6800aa60a9262632570ce5923cde59285d5321d
Bhendon Wesley <bhendonwesley@hotmail.com>
2020-08-24 23:08:12 -0300

    AC1

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -13,27 +13,3 @@ def main( ):
 
 if __name__ == "__main__":
 	main()
-<<<<<<< HEAD
-o conteudo do arquivo testes.py é 
-import jogovelha
-import sys
-
-erroInicializar = False
-jogo = jogovelha.inicializar()
-
-if len(jogo) != 3:
-	erroInicializar = True
-else:
-	for linha in jogo:
-		if len(linha) != 3:
-			erroInicializar = True
-		else:
-			for elemento in linha:
-				if elemento != '.':
-					erroInicializar = True
-if erroInicializar:
-	sys.exit(1)
-else:
-	sys.exit(0) 
-=======
->>>>>>> b62f11c8e45e5452e25230b9b0efc0991fcd043a
